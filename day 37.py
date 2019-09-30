cc733ea0199a9691493deb3f1ab39d06005b8e38
Malbshri <53739237+Malbshri@users.noreply.github.com>
2019-09-30 11:51:44 +0300

    week6a

---
+++ day 37.py
@@ -0,0 +1,20 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # week 6
+>>> # day 37
+>>> 
+>>> cars = ["Ford", "Volvo", "BMW"]
+>>> print(cars)
+['Ford', 'Volvo', 'BMW']
+>>> x = cars[0]
+>>> print(x)
+Ford
+>>> cars[0] = "Toyota"
+>>> print(cars)
+['Toyota', 'Volvo', 'BMW']
+>>> 
+>>> cars = ["Ford", "Volvo", "BMW"]
+>>> x = len(cars)
+>>> print(x)
+3
+>>> 
---
+++ day 38.py
@@ -0,0 +1,31 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # week 6
+>>> # day 38
+>>> 
+>>> cars = ["Ford", "Volvo", "BMW"]
+>>> for x in cars :
+	print(x)
+
+	
+Ford
+Volvo
+BMW
+>>> 
+>>> cars = ["Ford", "Volvo", "BMW"]
+>>> cars.append("Honda")
+>>> print(cars)
+['Ford', 'Volvo', 'BMW', 'Honda']
+>>> 
+>>> cars = ["Ford", "Volvo", "BMW"]
+>>> cars.pop(1)
+'Volvo'
+>>> print(cars)
+['Ford', 'BMW']
+>>> 
+>>> 
+>>> cars = ["Ford", "Volvo", "BMW"]
+>>> cars.remove("Volvo")
+>>> print(cars)
+['Ford', 'BMW']
+>>> 
